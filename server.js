35a32bf3dbba8ea29dbaf985f75a31160162db8d
mostafakjdeitawi <mostafagedo0785@gmail.com>
2021-04-06 23:37:55 +0300

    adding a database to our local machine

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const cors = require ('cors');
 
 const pg = require ('pg');
 
-const client = new pg.Client({process.env.DATABASE_URL,ssl: { rejectUnauthorized: false }});
+const client = new pg.Client({connectionString: process.env.DATABASE_URL,ssl: { rejectUnauthorized: false }});
 
 const superagent = require ('superagent');
 
